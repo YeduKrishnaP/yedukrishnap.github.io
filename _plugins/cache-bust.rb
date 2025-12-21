20cd14616fb3b93aacdbeb388737e57cc36f2e2e
Yedu Krishna <98282751+YeduKrishnaP@users.noreply.github.com>
2025-12-21 20:51:36 +0530

    T_T

--- _plugins/cache-bust.rb
+++ _plugins/cache-bust.rb
@@ -26,7 +26,13 @@ module Jekyll
 
       def file_content
         local_file_name = file_name.slice((file_name.index('assets/')..-1))
-        File.read(local_file_name)
+        if File.exist?(local_file_name)
+          File.read(local_file_name)
+        else
+          # Return empty string if file doesn't exist to avoid build errors
+          # This allows the build to continue even if an image reference is incorrect
+          ''
+        end
       end
 
       def file_contents
